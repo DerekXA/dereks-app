077681c010ab95ee8b04e6346a604fb5cc2cbcdd
DerekXA <dxayala03@gmail.com>
2017-11-01 18:55:28 -0700

    boo
    dab

---
+++ RatingControl.swift
@@ -0,0 +1,21 @@
+
+
+import UIKit
+
+class RatingControl: UIStackVeiw {
+
+}
+
+    //MARK: Initialization
+
+self.init(frame: CGRect){
+    
+    override; self.init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required; self.init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+   
+}
--- ViewController.swift
+++ ViewController.swift
@@ -25,7 +25,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     //MARK: Actions
-    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    @IBAction func selectImageFromPhotoLibrary(_ sender: Any) {
+    
+   
         
         // Hide the keyboard.
         nameTextField.resignFirstResponder()
@@ -35,26 +37,18 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         // Only allow photos to be picked, not taken.
         imagePickerController.sourceType = .photoLibrary
-       
-        // Make sure ViewController is notified when the user picks an image.
-        imagePickerController.delegate = self
-      
-        // Make sure ViewController is notified when the user picks an image.
-        imagePickerController.delegate = self
         
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
-    
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
     
     //MARK: UIImagePickerControllerDelegate
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        // Dismiss the picker if the user canceled.
-        dismiss(animated: true, completion: nil)
-    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         // The info dictionary may contain multiple representations of the image. You want to use the original.
         guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
@@ -66,5 +60,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Dismiss the picker.
         dismiss(animated: true, completion: nil)
     }
-}
+
+    }
+    
+
+   
+    
+
 
